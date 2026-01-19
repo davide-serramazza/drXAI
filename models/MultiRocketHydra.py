42dcaec330e8b2b94f827f278c8fb6bb51a1bd8c
davide <davide.serramazza@gmail.com>
2026-01-19 17:24:53 +0100

    MultiRocketHydra class updated

--- models/MultiRocketHydra.py
+++ models/MultiRocketHydra.py
@@ -1,5 +1,4 @@
 import numpy as np
-from sklearn.linear_model import RidgeClassifierCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 
@@ -17,6 +16,9 @@ from models.aaltd2024.code.utils import Dataset
 
 
 class dummy_transform():
+	""""
+	dummy transform i.e. f(X) = X for RidgeClassifier
+	"""
 	def __init__(self, X):
 		self.X = X
 		self.num_features = X.shape[1]
@@ -26,6 +28,11 @@ class dummy_transform():
 
 
 class MultiRocketHydra():
+	"""
+	implementation of MultiRocketHydra that
+	 1) allow to select hyper-parameters for both MultiRocket and Hydra
+	 2) uses RidgeClassifier's torch implementation that overcome limitation of sklearn's one
+	"""
 
 	def __init__(self,
 			hydra_params = {},
@@ -41,30 +48,32 @@ class MultiRocketHydra():
 				   ('scaler',StandardScaler())]
 		)
 
-		#self.clf = RidgeClassifierCV(
-		#	alphas=np.logspace(-3, 3, 10)
-		#)
-		self.clf = None
+		self.clf = None	#temporally set to None
 
 		super().__init__()
 
 	def fit(self,X,y):
+		"""
+
+		:param X: data to classify
+		:param y: labels
+		:return:
+		"""
+
+		# transform data using both hydra and MultiRocket, then concatenate the two representations
 		Xt_hydra  = self.hydra.fit_transform(X)
 		Xt_multiRocket = self.multiRocket.fit_transform(X)
-
 		Xt_total = np.concatenate([Xt_hydra,Xt_multiRocket],axis=1)
 
-		#dt = dummy_transform(Xt_total)
+		# Instantiate torch's RidgeClassifier, create a data loader and finally fit the model
 		self.clf = RidgeClassifier(dummy_transform(Xt_total))
-
 		train_loader = Dataset(Xt_total,y,batch_size=X.shape[0])
-
 		self.clf.fit(train_loader)
-		#self.clf.fit(Xt_total,y)
 
 		return self
 
 	def _predict(self,X,y) -> np.ndarray:
+
 		# TODO understand this warning "/home/davide/miniconda3/envs/train_aeon_clfs/lib/python3.13/site-packages/sklearn/pipeline.py:61: FutureWarning: This Pipeline instance is not fitted yet. Call 'fit' with appropriate arguments before using other methods such as transform, predict, etc. This will raise an error in 1.8 instead of the current warning.
 		Xt_hydra  = self.hydra.transform(X)
 		Xt_multiRocket = self.multiRocket.transform(X)
