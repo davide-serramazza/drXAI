d6b12a2dd97f0235d697dcde5cff671eeb1e11d1
davide <davide.serramazza@gmail.com>
2026-01-22 12:32:30 +0100

    train_classifiers.py can now handle train on selected featues

--- train_classifiers.py
+++ train_classifiers.py
@@ -2,14 +2,27 @@
 #from utils.trainers_aeon import train
 from utils.trainers import train
 from utils.data_utils import load_datasets
-from utils.helpers import str2bool, elapsed_time, save_model
+# TODO should I move some functions in other files?
+from utils.helpers import elapsed_time, save_model, str2bool, get_computed_AI_selections, extract_timePoints
 
 import argparse
 import os
 import gc
 import numpy as np
+from copy import deepcopy
 
-#TODO clean and comment
+
+# TODO move elsewhere
+def extract_features(data, selection,channel_selection):
+	if channel_selection:
+		data['train_set']['X'] = data['train_set']['X'][:,selection,:]
+		data['test_set']['X'] = data['test_set']['X'][:,selection,:]
+	else:
+		data['train_set']['X'], data['test_set']['X'] = extract_timePoints(data,selection)
+
+	return data
+
+
 
 def main(args):
 	base_path = args.dataset_dir
@@ -22,61 +35,82 @@ def main(args):
 	if all_clfs_allowed == False : raise ValueError("invalid classifier names")
 
 	results_file = args.result_file
+	selected_features = args.selection_dir
+	channel_selection = args.channel_selection
+	# TODO check selected features without channel selection
 
 	results = {}
 
-	for f in sorted(os.listdir(args.dataset_dir ) ):
+	for f in sorted(os.listdir(args.dataset_dir ) )[1:]:
 
 		dataset_dir = os.path.join(base_path,f)
-		data = load_datasets(dataset_dir, f)
-		print("\n\n current loaded dataset is....", data['name'])
+		original_data = load_datasets(dataset_dir, f)
+		current_dataset_name = original_data['name']
+		print("\n\n current loaded dataset is....", original_data['name'])
 
-		current_dataset = data['name']
-		results[current_dataset] = {}
+		selected_features_file = os.path.join(selected_features, current_dataset_name + "_results.npz")
+		selections = get_computed_AI_selections(
+			saliency_map_dict=np.load(selected_features_file,allow_pickle=True)['results'].item(),
+			# TODO remove hydra level from selection_dict????
+			channel_sel=channel_selection,selection_dict={'hydra':{}}, info="") if selected_features else \
+			{'hydra': {'allFeatures':None }}
 
-		for model_name in model_names:
 
-			# TODO remove batch_size from here, use the one in model.ConvTran.py (default_hyperparams)
-			best_accuracy = -1
-			story = {
-				'accuracy' : [],
-				'average_memory_GB' : [],
-				'peak_memory_GB' :[],
-				'training_time' : []
-			}
+		results[current_dataset_name] = {}
 
-			for i in range(5):
-				# TODO use **kwargs to say which value is which param?
-				model, current_accuracy, mem_used, training_time = elapsed_time(
-					train,(data, model_name,False) )
+		# TODO to remove hydra level from selection_dict????
+		for selection_name, selected_f in selections['hydra'].items():
 
-				story['accuracy'].append(current_accuracy)
-				story['average_memory_GB'].append(mem_used['average_memory_GB'])
-				story['peak_memory_GB'].append(mem_used['peak_memory_GB'])
-				story['training_time'].append(training_time)
+			data = original_data if selection_name=="allFeatures" else \
+				extract_features(deepcopy(original_data) , selected_f,channel_selection)
 
-				print(i,")",model_name,"training over! Accuracy is: ",current_accuracy, "\tTraining time:", training_time)
-				if current_accuracy > best_accuracy:
-					current_accuracy = best_accuracy
+			print("current evaluated selection is", selection_name)
 
-					# save current best model
-					file_name = "_".join((current_dataset,model_name,"allFeatures"))
-					save_model(file_name, model, model_name, saved_models_dir)
+			for model_name in model_names:
 
-				# delete model and run garbage collector for memory tracking purposes
-				del model
-				gc.collect()
+				results[current_dataset_name][model_name] = {}
 
-			# add current results to results data structure
-			results[current_dataset][model_name] = {
-				'accuracy' : np.mean(story['accuracy']),
-				'average_memory_GB' : np.mean(story['average_memory_GB']),
-				'peak_memory_GB' : np.max(story['peak_memory_GB']),
-				'training_time' : np.mean(story['training_time']),
-				'story' : story,
-			}
+				best_accuracy = -1
+				story = {
+					'accuracy' : [],
+					'average_memory_GB' : [],
+					'peak_memory_GB' :[],
+					'training_time' : []
+				}
 
-		np.save(results_file, results)
+				for i in range(5):
+					print("training",(i+1),"-th model ...")
+					# TODO use **kwargs to say which value is which param?
+					model, current_accuracy, mem_used, training_time = elapsed_time(
+						train,(data, model_name,False) )
+
+					story['accuracy'].append(current_accuracy)
+					story['average_memory_GB'].append(mem_used['average_memory_GB'])
+					story['peak_memory_GB'].append(mem_used['peak_memory_GB'])
+					story['training_time'].append(training_time)
+
+					print(i,")",model_name,"training over! Accuracy is: ",current_accuracy, "\tTraining time:", training_time)
+					if current_accuracy > best_accuracy:
+						current_accuracy = best_accuracy
+
+						# save current best model
+						file_name = "_".join((current_dataset_name, model_name, "allFeatures"))
+						save_model(file_name, model, model_name, saved_models_dir)
+
+					# delete model and run garbage collector for memory tracking purposes
+					del model
+					gc.collect()
+
+				# add current results to results data structure
+				results[current_dataset_name][model_name] = {
+					'accuracy' : np.mean(story['accuracy']),
+					'average_memory_GB' : np.mean(story['average_memory_GB']),
+					'peak_memory_GB' : np.max(story['peak_memory_GB']),
+					'training_time' : np.mean(story['training_time']),
+					'story' : story,
+				}
+
+			np.save(results_file, results)
 
 
 if __name__ == '__main__':
@@ -85,8 +119,10 @@ if __name__ == '__main__':
 	parser.add_argument("saved_models_path", type=str, help="folder where to save models")
 	parser.add_argument("result_file", type=str, help=".npy file where to store results")
 	parser.add_argument("--classifiers", nargs='+',help="classifier names")
-
 	#TODO implement this!
-	parser.add_argument("--initial_selection", type=str2bool, default=False)
+	parser.add_argument("--selection_dir", type=str, default=None, help='feature selection(s) to be used. '
+																		'If not provided all features are used.')
+	parser.add_argument('--channel_selection',type=str2bool, default=False, help="whether to perform "
+															"channel selection(True) or time point selection(False)")
 	args = parser.parse_args()
 	main(args)
